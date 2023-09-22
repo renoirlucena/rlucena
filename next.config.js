869df82c018d8676183a67a1455a9653b096becf
Renoir Lucena <renoirlucena@gmail.com>
2023-09-22 12:38:42 -0500

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/[repo-name]' : '',
+};
